77f3d1a279d4ce26d42cf76e83639f0149810f2a
hikaru-nakayama <hikaru-nakayama@c-fo.com>
2024-02-03 17:40:50 +0900

    Implement simple repl

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "monkey/lexer"
+import (
+	"fmt"
+	"monkey/repl"
+	"os"
+	"os/user"
+)
 
 func main() {
-	lexer.TestNextToken()
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey programing language\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+
 }
